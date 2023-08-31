e0fd6800e2c7a2eb400e897756a7945ad44144a5
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-08-31 11:07:09 +0530

    Update hehe.py

--- hehe.py
+++ hehe.py
@@ -1,65 +1,20 @@
-import logging
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
+from telegram import Bot
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 BOT_TOKEN = '6286222522:AAGDmZF5xdpakB8_4-SpmATSjerBVG4iohs'
 
-# Replace 'TARGET_USERNAME' with the username of the user to be promoted
-TARGET_USERNAME = '@Harima_Nagar_1'
-
-# Configure logging
-logging.basicConfig(filename='bot.log', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-CHANNEL_ID = 0  # Global variable to store the channel ID
-
-# Conversation states
-CHOOSE_CHANNEL = range(1)
-
-def start(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text("Send me the channel ID where you want to be promoted.")
-    return CHOOSE_CHANNEL
-
-def choose_channel(update: Update, context: CallbackContext) -> None:
-    global CHANNEL_ID
-    CHANNEL_ID = int(update.message.text)
-
-    user_id = update.message.from_user.id
-    user_username = update.message.from_user.username
-    
-    # Check if the user is authorized
-    if user_username == TARGET_USERNAME:
-        context.bot.promote_chat_member(CHANNEL_ID, user_id, can_change_info=True, can_invite_users=True)
-        update.message.reply_text("You've been promoted! 🎉")
-    else:
-        update.message.reply_text("You are not authorized for promotion.")
-    
-    return ConversationHandler.END
+# Replace with the actual user ID and channel ID
+USER_ID = 5151264430
+CHANNEL_ID = -1001626206283
 
 def main():
-    updater = Updater(token=BOT_TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
-
-    # Create a conversation handler
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('okpromoteme', start)],
-        states={
-            CHOOSE_CHANNEL: [MessageHandler(Filters.text, choose_channel)],
-        },
-        fallbacks=[],
-    )
+    bot = Bot(token=BOT_TOKEN)
     
-    dispatcher.add_handler(conv_handler)
-
-    # Log all errors to a file
-    dispatcher.add_error_handler(error_handler)
-
-    updater.start_polling()
-    updater.idle()
-
-def error_handler(update: Update, context: CallbackContext):
-    """Log the error caused by an update."""
-    logging.error(f"Update {update} caused error {context.error}")
+    try:
+        bot.promote_chat_member(CHANNEL_ID, USER_ID, can_change_info=True, can_invite_users=True)
+        print(f"User with ID {USER_ID} promoted in channel with ID {CHANNEL_ID}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 if __name__ == '__main__':
     main()
